f4023dbb949a305a3bf73d7db5f5d54b2d2506a5
Irian3x3 <irian3x3@gmail.com>
2020-11-10 08:04:42 -0400

    help was empty so i had to update it

--- src/main/commands/info/help.js
+++ src/main/commands/info/help.js
@@ -1,8 +1,15 @@
+const d = require("discord.js");
+
 module.exports = {
     name: "help",
     description: "Provides a full list of commands.",
     aliases: ['commands', 'cmds', "h"],
     async run(message, args, bot) {
-        
+        const em = new d.MessageEmbed()
+        .setAuthor(`Help has arrived!`, `${bot.user.displayAvatarURL({format:`png`})}`)
+        .setDescription(`\`<>\` - Optional\n \`[]\` - Required\n\n**Fun**:\n\`hug [user]\`: Hugs someone\n\`pat [user]\`: Pats someone\n\`say|echo [to say]\`: Makes the bot say something.\n\`wink\`: Wink.\n\n**Info**:\n\`botinfo|bot-info|bot|infobot|info-bot|ib|bi\`: Provides information and statistics on the bot.\n\`help|cmds|commands|h\`: This!\n\`links\`: Provides links for the bot\n\`ping\`: Provides bot's ping/latency in milliseconds.\n\n**Util**:\n\`whois <user>\`: Provides information about a certain user.\n\`serverinfo|server|si\`: Provides information and statistics about a server.\n\n**Other**:\n\`covid\`: Provides global COVID-19 stats.`)
+        .setColor('RANDOM')
+        .setThumbnail('https://cdn.discordapp.com/attachments/774010416225452104/775388719272165417/red-spinny-thing.gif');
+        message.channel.send(em)
     }
 }
